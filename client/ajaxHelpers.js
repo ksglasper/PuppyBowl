c2299353d866e91182b9a3d2467b61521e0e5a5c
Kyle Glasper <ksglasper@gmail.com>
2022-07-02 13:08:27 -0500

    created fetch all player function

--- client/ajaxHelpers.js
+++ client/ajaxHelpers.js
@@ -1,10 +1,19 @@
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
-const cohortName = 'COHORT-NAME';
+const cohortName = '2206-FTB-ET-WEB-FT'
 // Use the APIURL variable for fetch requests
 const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
 
 
 export const fetchAllPlayers = async () => {
+    try {
+        const response = await fetch(APIURL + 'players/')
+        const result = await response.json()
+        console.log(result.data)
+        return result.data.players 
+    } catch (error) {
+        console.error(error, 'something broke')
+    }
+    
 
 };
 
